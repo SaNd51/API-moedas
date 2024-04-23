17ed111ccdec65de23baffd1424b6d49e85b5299
sander luan <sanderluan51@gmail.com>
2024-04-23 03:35:38 -0300

    teste

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,33 @@
+document.querySelector('.search').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const moeda = document.querySelector('#nome').value;
+    console.log(moeda)
+
+    if (!moeda) {
+        document.querySelector("#info").classList.remove('show');
+        showAlert('Você precisa digitar uma moeda...');
+        return;
+    }
+
+    const apiUrl = `https://economia.awesomeapi.com.br/json/last/${encodeURI(moeda)}`;
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+    for (const x in json){
+        valor = x
+    }
+
+    showInfo({
+        cotaçao: json[valor]['bid']
+    })
+});   
+    
+function showInfo(json){
+    showAlert('');
+    document.querySelector('#title').innerHTML = `R$ ${json.cotaçao}`;
+}
+
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
+}
