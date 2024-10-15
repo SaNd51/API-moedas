33289a6bc970e7d748d147643bcf12acedcb01ea
SaNd51 <sanderluan51@gmail.com>
2024-10-15 02:52:45 -0300

    update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,27 +1,39 @@
 document.querySelector('.search').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const moeda = document.querySelector('#nome').value;
-    console.log(moeda)
-
-    if (!moeda) {
+    const moeda1 = document.querySelector('#val1').value;
+    const moeda2 = document.querySelector('#val2').value;
+    const qtd = document.querySelector('#valor').value;
+    
+    if (!qtd){
         document.querySelector("#info").classList.remove('show');
-        showAlert('Você precisa digitar uma moeda...');
+        showAlert('Você precisa digitar um número!');
         return;
     }
-
-    const apiUrl = `https://economia.awesomeapi.com.br/json/last/${encodeURI(moeda)}`;
+    
+    const apiUrl = `https://economia.awesomeapi.com.br/json/last/${encodeURI(moeda1)}-${encodeURI(moeda2)}`;
+    console.log(apiUrl)
 
     const results = await fetch(apiUrl);
     const json = await results.json();
+    console.log(json)
     for (const x in json){
         valor = x
     }
     showInfo({
-        cotaçao: json[valor]['bid']
+        soma: json[valor]['bid']*qtd,
     })
 }); 
 
 function showInfo(json){
-    document.querySelector('#title').innerHTML = `R$ ${json.cotaçao}`;
+    if(json.soma){
+        document.querySelector('#title').innerHTML = `Resultado: ${json.soma}`;
+    }else{
+        document.querySelector('#title').innerHTML = `Selecione uma moeda diferente`;
+    }
 }
+
+function showAlert(text){
+    document.querySelector('#title').innerHTML = `${text}`;
+}
+ 
